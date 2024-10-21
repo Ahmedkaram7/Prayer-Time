4cf5dc53c37a6ca5b35aafa4d8710a226ab7b4fb
Ahmed Karam <ahmedkaramali710@outlook.com>
2024-10-22 01:59:05 +0300

    Build Prayer Time Calculation Methods with HTML,CSS and JS

---
+++ main.js
@@ -0,0 +1,41 @@
+// https://api.aladhan.com/v1/timingsByAddress/21-10-2024?address=Cairo,EGP&method=5
+
+let apiUrl = "https://api.aladhan.com/v1/timingsByAddress/21-10-2024?address=";
+
+let changeCity = document.querySelector(".btn-save");
+let input = document.querySelector(".input input");
+let timezone = document.querySelector(".city");
+
+async function times(city) {
+  let response = await fetch(apiUrl + city + ",EGP&method=5");
+  console.log(response);
+  let data = await response.json();
+  console.log(data);
+  console.log(data.data.timings.Fajr);
+
+  let fajr = document.querySelector(".fajr-time");
+  let dhuhr = document.querySelector(".dhuhr-time");
+  let sunrise = document.querySelector(".sunrise-time");
+  let asr = document.querySelector(".asr-time");
+  let maghrib = document.querySelector(".maghrib-time");
+  let isha = document.querySelector(".isha-time");
+
+  fajr.innerText = data.data.timings.Fajr;
+  dhuhr.innerText = data.data.timings.Dhuhr;
+  sunrise.innerText = data.data.timings.Sunrise;
+  maghrib.innerText = data.data.timings.Maghrib;
+  isha.innerText = data.data.timings.Isha;
+  asr.innerText = data.data.timings.Asr;
+  timezone.innerText = `timezone: ${data.data.meta.timezone}`;
+}
+
+times("Cairo");
+
+changeCity.addEventListener("click", () => {
+  if (input.value === "") {
+    alert("Enter City");
+  } else {
+    times(input.value);
+  }
+  input.value = "";
+});
